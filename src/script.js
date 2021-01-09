757f8bec118487562991dc92cbf8746b125c06e3
Konixy <66780340+Konixy@users.noreply.github.com>
2021-01-09 18:18:05 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,13 +1,41 @@
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0)
+    header.classList.toggle("sticky", window.scrollY > 0);
 })
 
-const burger = document.querySelector(".burger");
+function toggleMenu(){
+    var header = document.querySelector("header");
+    var menuToggle = document.querySelector('.burger');
+    var menu = document.querySelector('.menu');
+    var all = document.querySelector('html');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+    // header.classList.toggle('sticky');
+    // all.classList.toggle('cantscroll');
+}
 
-burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-})
+function toggle(){
+    var header = document.querySelector("header");
+    var blur = document.querySelector('#blur');
+    var all = document.querySelector('html');
+    blur.classList.toggle('active');
+    header.classList.toggle('sticky2');
+    all.classList.toggle('cantscroll');
+    var connect = document.querySelector('#connect');
+    connect.classList.toggle('active');
+}
+
+function validate(){
+    const form = document.getElementsById('email');
+    const email = document.getElementsById('email').value;
+    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+
+    if(email.match(pattern)){
+        form.classList.add('valid')
+    } else {
+        form.classList.remove('valid')
+    }
+}
 
 //$(document).ready(function(e){
 //    $('.btn').on('mouseenter', function(e){
